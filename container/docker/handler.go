763753a63031f1a3c974b665e0989ca6312310d0
Daniel Fuentes <daniel.fuentes@clever.com>
2015-05-05 14:26:31 -0700

    strip initial forward slash from app id

--- container/docker/handler.go
+++ container/docker/handler.go
@@ -363,5 +363,8 @@ func getMarathonAppName(ctnr *docker.Container) string {
 }
 
 func sanitizeMarathonAppID(appID string) string {
+	if strings.HasPrefix(appID, "/") {
+		appID = appID[1:]
+	}
 	return strings.Replace(appID, "/", "-", -1)
 }
